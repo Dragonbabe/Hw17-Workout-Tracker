cf80da150362444ab3f92209d087c1bfc73e60ce
Esther Walker <equestriennesrule@yahoo.com>
2020-03-25 21:30:15 -0700

    deleted  json files; updated 1; err msg for server

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const express = require(`express`);
 
 const mongojs = require(`mongojs`);
 
+const logger = require(`morgan`);
+
 const app = express();
 
 const dbName = `workouts`;
 
 const collections = [`exercises`];
+
+const db = mongojs(dbName, collections);
+
+db.on(`error`, error => {
+    console.log(`Database Error:`, error);
+});
+
+app.get(`/all`,)
